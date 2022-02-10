ae3917f1753132cdf6900f8e8b55e7bd14ff57b9
Jenna Ransom <jenna.ransom@gmail.com>
2022-02-10 15:07:29 -0800

    add randomness to color picking and degree for gradient

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ let layer1 = document.querySelector('div.layer1')
 let layer2 = document.querySelector('div.layer2')
 
 function makeColor() {
-  let h = 50 
-  let s = 50 
-  let l = 50
+  let h = Math.random() * 360 
+  let s = 40 + Math.random() * 50
+  let l = 50 + Math.random() * 50
   return `hsl(${h}, ${s}%, ${l}%)`
 }
 
 function makeGradient() {
   let c1 = makeColor()
   let c2 = makeColor()
-  let angle = 45
+  let angle = Math.random() * 360
   return `linear-gradient(${angle}deg, ${c1}, ${c2})`
 }
 
